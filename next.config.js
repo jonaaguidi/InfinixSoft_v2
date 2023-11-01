af421997e4afda5c3ee5b9766cd808b2c40ebcd2
Jonathan Guidi <jguidi.info@gmail.com>
2023-11-01 15:04:18 -0300

    changes in config next.config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack(config, { isServer }) {
-      const prefix = config.assetPrefix ?? config.basePath ?? '';
-      config.module.rules.push({
-        test: /\.mp4$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            publicPath: `${prefix}/_next/static/media/`,
-            outputPath: `${isServer ? '../' : ''}static/media/`,
-            name: '[name].[hash].[ext]',
-          },
-        }],
-      });
-  
-      return config;
+        const prefix = config.assetPrefix ?? config.basePath ?? '';
+        config.module.rules.push({
+            test: /\.mp4$/,
+            use: [{
+                loader: 'file-loader',
+                options: {
+                    publicPath: `${prefix}/_next/static/media/`,
+                    outputPath: `${isServer ? '../' : ''}static/media/`,
+                    name: '[name].[hash].[ext]',
+                },
+            }],
+        });
+
+        return config;
     },
     experimental: {
-      webpackBuildWorker: true,
+        webpackBuildWorker: true,
     },
-  };
-  
-  module.exports = nextConfig;
-  
+    eslint: { 
+        ignoreDuringBuilds: true,
+    }
+};
+
+module.exports = nextConfig;
