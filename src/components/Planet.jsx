dbaa222fde17a6fe09817f024a30d638f95c2303
Jonathan Guidi <jguidi.info@gmail.com>
2023-11-09 15:09:23 -0300

    test planet

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -18,14 +18,15 @@ const DraggablePlanet = () => {
 
   useEffect(() => {
     let phi = 0;
+    let currentCanvasRef = canvasRef.current; // Almacenar el valor del ref
 
     const onRender = (state) => {
       state.phi = phi;
-      phi += 0.0035;
+      phi += 0.0015;
       state.phi += r.get();
     };
 
-    const globe = createGlobe(canvasRef.current, {
+    const globe = createGlobe(currentCanvasRef, {
       devicePixelRatio: 2,
       width: 600 * 2,
       height: 600 * 2,
@@ -34,7 +35,7 @@ const DraggablePlanet = () => {
       dark: 1,
       diffuse: 3,
       scale: 0.8,
-      mapSamples: 9000,
+      mapSamples: 7000,
       mapBrightness: 8,
       baseColor: [0.3, 0.3, 0.3],
       markerColor: [0.8588, 0.2235, 0.3412],
@@ -42,7 +43,11 @@ const DraggablePlanet = () => {
       markers: [
         { location: [25.276987, 55.296249], size: 0.06 },  // Dubai
         { location: [25.7617, -80.1918], size: 0.06 },    // Miami
-        // ... (otros marcadores)
+        { location: [40.7128, -74.006], size: 0.06 },     // New York
+        { location: [-34.6118, -58.4173], size: 0.06 },   // Buenos Aires
+        { location: [-23.5505, -46.6333], size: 0.06 },   // São Paulo
+        { location: [19.4326, -99.1332], size: 0.06 },    // Ciudad de México
+        { location: [13.6972, -89.1912], size: 0.06 },    // San Salvador
       ],
       onRender: (state) => {
         onRender(state);
@@ -51,17 +56,17 @@ const DraggablePlanet = () => {
 
     const handlePointerDown = (e) => {
       pointerInteracting.current = e.clientX - pointerInteractionMovement.current;
-      canvasRef.current.style.cursor = 'grabbing';
+      currentCanvasRef.style.cursor = 'grabbing';
     };
 
     const handlePointerUp = () => {
       pointerInteracting.current = null;
-      canvasRef.current.style.cursor = 'grab';
+      currentCanvasRef.style.cursor = 'grab';
     };
 
     const handlePointerOut = () => {
       pointerInteracting.current = null;
-      canvasRef.current.style.cursor = 'grab';
+      currentCanvasRef.style.cursor = 'grab';
     };
 
     const handleMouseMove = (e) => {
@@ -84,26 +89,26 @@ const DraggablePlanet = () => {
       }
     };
 
-    canvasRef.current.addEventListener('pointerdown', handlePointerDown);
-    canvasRef.current.addEventListener('pointerup', handlePointerUp);
-    canvasRef.current.addEventListener('pointerout', handlePointerOut);
-    canvasRef.current.addEventListener('mousemove', handleMouseMove);
-    canvasRef.current.addEventListener('touchmove', handleTouchMove);
+    currentCanvasRef.addEventListener('pointerdown', handlePointerDown);
+    currentCanvasRef.addEventListener('pointerup', handlePointerUp);
+    currentCanvasRef.addEventListener('pointerout', handlePointerOut);
+    currentCanvasRef.addEventListener('mousemove', handleMouseMove);
+    currentCanvasRef.addEventListener('touchmove', handleTouchMove);
 
     return () => {
       globe.destroy();
-      canvasRef.current.removeEventListener('pointerdown', handlePointerDown);
-      canvasRef.current.removeEventListener('pointerup', handlePointerUp);
-      canvasRef.current.removeEventListener('pointerout', handlePointerOut);
-      canvasRef.current.removeEventListener('mousemove', handleMouseMove);
-      canvasRef.current.removeEventListener('touchmove', handleTouchMove);
+      currentCanvasRef.removeEventListener('pointerdown', handlePointerDown);
+      currentCanvasRef.removeEventListener('pointerup', handlePointerUp);
+      currentCanvasRef.removeEventListener('pointerout', handlePointerOut);
+      currentCanvasRef.removeEventListener('mousemove', handleMouseMove);
+      currentCanvasRef.removeEventListener('touchmove', handleTouchMove);
     };
-  }, []);
+  }, [api, r]);
 
   return (
     <div className="relative z-0">
       <div className="absolute z-0 inset-0">
-        <div className="rounded-full bg-[#DB3957] blur-[128px] opacity-30 w-full h-full"></div>
+        <div className="rounded-full bg-[#DB3957] blur-[116px] opacity-30 w-full h-full"></div>
       </div>
       <canvas
         ref={canvasRef}
@@ -115,3 +120,4 @@ const DraggablePlanet = () => {
 };
 
 export default DraggablePlanet;
+
