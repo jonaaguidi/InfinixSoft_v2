3bb6e4d8d58005a78f1021138cec4a65b7ac91e0
Jonathan Guidi <infinix@MacBook-Air-de-JonaaGuidi.local>
2024-04-02 18:56:42 -0300

    test2

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,6 +40,19 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning lang="en">
+          <Script
+        onLoad={() => {
+          (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3929516,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        }}
+      />
+
       <Script async="" src="//connect.facebook.net/en_US/fbds.js" />
       <Script async="" src="https://www.google-analytics.com/analytics.js" />
       <Script src="https://js.hscollectedforms.net/collectedforms.js" type="text/javascript" id="CollectedForms-22404315" data-leadin-portal-id="22404315" data-leadin-env="prod" data-loader="hs-scriptloader" data-hsjs-portal="22404315" data-hsjs-env="prod" data-hsjs-hublet="na1" />
